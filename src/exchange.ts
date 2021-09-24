34bbf0c6147f5e256803770afc563114bc4da20a
Christian Sumido <gcbsumid@gmail.com>
2021-09-24 09:43:20 -0700

    Adding Exchange Subgraph abis, yaml, and graphql files

---
+++ src/exchange.ts
@@ -0,0 +1,5 @@
+import { log, ByteArray, BigInt, Address, crypto, store } from "@graphprotocol/graph-ts"
+import { ADDRESS_ZERO, ONE_BI, ZERO_BI } from "./constants";
+
+export function OrderPlaced(event: ContractsDeployedEvent): void {
+}
